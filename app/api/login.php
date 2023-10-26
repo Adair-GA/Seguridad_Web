abca7f37c174d4a82c69258e7a477bf7ddccd19c
Javier Arambarri Calvo <javierarambarricalvo.ehu@gmail.com>
2023-10-26 14:35:44 +0200

    introducido Salt and Hash al password

--- app/api/login.php
+++ app/api/login.php
@@ -3,6 +3,13 @@ include "../dbconn.php";
 $usuario = $_REQUEST['email'];
 $contrasena = $_REQUEST['password'];
 
+// Obtenemos la sal del usuario, ¿si hay más de un usuario con el mismo nombre?
+$query = "SELECT sal FROM usuarios WHERE (usuario = '$usuario' OR email = '$usuario')";
+$result = mysqli_query($conn, $query) or die (mysqli_error($conn));
+$row = mysqli_fetch_array($result);
+$contrasena .= $row[0]; // Concatenamos la contraseña introducida con la sal
+$contrasena = hash('sha512', $contrasena); // Obtenemos el hash correspondiente
+
 $query = "SELECT email, usuario, dni FROM usuarios WHERE (usuario = '$usuario' OR email = '$usuario') AND contraseña = '$contrasena'";
 $result = mysqli_query($conn, $query) or die (mysqli_error($conn));
 $row = mysqli_fetch_array($result);
--- app/api/signup_register.php
+++ app/api/signup_register.php
@@ -1,18 +1,37 @@
 <?php
 include "../dbconn.php";
 
+//password_hash() ?
+
+function getSalt($n) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+ 
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+ 
+    return $randomString;
+}
+
 // login.js línea 106, usamos método POST, tendremos que recibir body de alguna forma
 // se recibe el body a través de los $_REQUEST
 $nombre = $_REQUEST['name'];
 $apellido = $_REQUEST['surname'];
 $usuario = $_REQUEST['username'];
 $contrasena = $_REQUEST['password'];
+
+$salt = getSalt(6);
+$contrasena .= $salt;
+$contrasena = hash('sha512', $contrasena);
+
 $email = $_REQUEST['email'];
 $telefono = $_REQUEST['phone'];
 $fecha_nacimiento = $_REQUEST['dob'];
 $dni = $_REQUEST['dni'];
 
-$query = "INSERT INTO usuarios (dni, nombre, apellidos, usuario, contraseña, email, telefono, fecha_nacimiento) VALUES ('$dni', '$nombre', '$apellido', '$usuario', '$contrasena', '$email', '$telefono', '$fecha_nacimiento')";
+$query = "INSERT INTO usuarios (dni, nombre, apellidos, usuario, contraseña, sal, email, telefono, fecha_nacimiento) VALUES ('$dni', '$nombre', '$apellido', '$usuario', '$contrasena', '$salt', '$email', '$telefono', '$fecha_nacimiento')";
 $result = mysqli_query($conn, $query) or die (mysqli_error($conn));
 if ($result) { // Si hay resultado, es decir, si se ha podido actualizar, todo correcto
     echo "Usuario registrado correctamente";
