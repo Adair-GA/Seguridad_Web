79741dd963891cf3b5af281f1a0132d8db9772bd
Adair Gondan <adairyves@gmail.com>
2023-11-19 23:35:55 +0100

    Mensaje de error si el usuario o la contraseña son incorrectos

--- app/api/login.php
+++ app/api/login.php
@@ -35,15 +35,25 @@ if (/*$data && */hash_equals($token, $_SESSION['token'])){
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_array($result);
+    if ($row == null || $row == false){
+        header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed');
+        echo "Error: Usuario incorrecto";
+        exit;
+    }
     $contrasena .= $row[0]; // Concatenamos la contraseña introducida con la sal
     $contrasena = hash('sha512', $contrasena); // Obtenemos el hash correspondiente
-
+    
     $query = "SELECT email, usuario, dni FROM usuarios WHERE (usuario = ? OR email = ?) AND contraseña = ?";
     $stmt = mysqli_prepare($conn, $query) or die (mysqli_error($conn));
     mysqli_stmt_bind_param($stmt, "sss", $usuario, $email, $contrasena);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_array($result);
+    if ($row == null || $row == false){
+        header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed');
+        echo "Error: Contraseña incorrecta";
+        exit;
+    }
     if ($row[0]) {
         $_SESSION['email'] = $row[0];
         $_SESSION['usuario'] = $row[1];
